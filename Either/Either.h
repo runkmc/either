3f6edd3c0b1cc448ab1efb46616679018aeef14b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-06-06 12:39:20 -0700

    Start from scratch

---
+++ Either/Either.h
@@ -0,0 +1,19 @@
+//
+//  Either.h
+//  Either
+//
+//  Created by Kevin McGladdery on 6/6/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Either.
+FOUNDATION_EXPORT double EitherVersionNumber;
+
+//! Project version string for Either.
+FOUNDATION_EXPORT const unsigned char EitherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Either/PublicHeader.h>
+
+
--- Example/Pods/Target Support Files/Pods-Either_Tests/Pods-Either_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Either_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Either_TestsVersionString[];
-
