70d1a9ceeedcf3ebebf3ae192026539cb6e5e20e
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-06-06 12:42:23 -0700

    Create new Either files

---
+++ Either/Either.swift
@@ -0,0 +1,12 @@
+//
+//  Either.swift
+//  Either
+//
+//  Created by Kevin McGladdery on 6/6/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+
+public enum Either<T, U> {
+}
